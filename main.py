ec171947de2125f6cebebbdd6c268a15f4464c01
SpaceshipxDev <150497663+SpaceshipxDev@users.noreply.github.com>
2023-12-01 16:02:11 +1100

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,13 @@ from langchain.schema import (
 
 def init():
     # Load the OpenAI API key from the environment variable
-    load_dotenv()
-    
-    # test that the API key exists
-    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-        print("OPENAI_API_KEY is not set")
-        exit(1)
-    else:
-        print("OPENAI_API_KEY is set")
 
     # setup streamlit page
     st.set_page_config(
         page_title="Your own ChatGPT",
         page_icon="🤖"
     )
+    os.environ['OPENAI_API_KEY'] = st.secrets['da_key']
 
 
 def main():
